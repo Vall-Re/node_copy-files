d1300dd63143960465372765109b954a5995ca55
Svitlana Chepa <svetachepa2320@gmail.com>
2025-12-24 09:58:37 +0000

    Fix task solution

--- tests/copyFiles.test.js
+++ tests/copyFiles.test.js
@@ -84,7 +84,7 @@ describe('File Copy', () => {
     expect(fs.existsSync(destinationFile)).toBe(false);
   });
 
-  test('should throw an error if destination is a directory', async () => {
+  test('should copy file into a directory', async () => {
     const directoryPath = path.join(
       tempDir,
       faker.system.commonFileName('txt'),
